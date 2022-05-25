1758b6efccdce2554961e43593226fe8d86ecfbe
Iago Barboza <iagobarboza@gmail.com>
2022-05-24 22:37:57 -0300

    feat: add the menu logo

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,10 +1,13 @@
 import styles from './Menu.module.scss'
+// import LogoSvg from '../../assets/logo.svg';
+import { ReactComponent as Logo } from '../../assets/logo.svg'
 
 export default function Menu() {
   return (
     <main>
-      <nav className="menu">
-
+      <nav className={styles.menu}>
+        {/* <img src={Logo} alt="aluroni logo" /> */}
+        <Logo />
       </nav>
     </main>
   )
