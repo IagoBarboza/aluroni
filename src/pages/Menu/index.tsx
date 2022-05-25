bfcd659e16b04edee177fa20934896f852bd8728
Iago Barboza <iagobarboza@gmail.com>
2022-05-25 16:11:34 -0300

    refact: import the logo as a react component feat: reset css

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,6 +1,6 @@
 import styles from './Menu.module.scss'
 // import LogoSvg from '../../assets/logo.svg';
-import { ReactComponent as Logo } from '../../assets/logo.svg'
+import { ReactComponent as Logo } from 'assets/logo.svg'
 
 export default function Menu() {
   return (
