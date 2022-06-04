615fc70a7a5c8b9df339eda5d03893a42407ff0f
Iago Barboza <iagobarboza@gmail.com>
2022-06-04 14:49:33 -0300

    feat: add the images of the items

--- src/pages/Menu/List/Item/index.tsx
+++ src/pages/Menu/List/Item/index.tsx
@@ -1,15 +1,16 @@
 import styles from './Item.module.scss'
 import items from '../items.json'
+import classnames from 'classnames'
 
 type ItemProps = typeof items[0]
 
 export default function Item(props: ItemProps) {
-  const { title, description, category, size,  serving, price } = props 
+  const { title, description, photo, category, size,  serving, price } = props 
 
   return (
     <div className={styles.item}>
       <div className={styles.item__image}>
-        <img src="" alt={title} />
+        <img src={photo} alt={title} />
       </div>
 
       <div className={styles.item__description}>
@@ -19,7 +20,14 @@ export default function Item(props: ItemProps) {
         </div>
 
         <div className={styles.item__tags}>
-          <div className={styles.item__category}>{category.label}</div>
+          <div
+            className={classnames({
+              [styles.item__category]: true,
+              [styles[`item__category__${category.label.toLowerCase()}`]]: true
+            })}
+          >
+            {category.label}
+          </div>
           <div className={styles.item__size}>{size}g</div>
           <div className={styles.item__serving}>Serve {serving} pessoa{serving > 1 ? 's' : ''}</div>
           <div className={styles.item__price}>R$ {price.toFixed(2)}</div>
